d7ef70097b70d301623070071733b87ca9a6e09d
Keanu Lee <keanulee@google.com>
2018-07-02 16:31:36 -0700

    Formatter

--- paper-progress.js
+++ paper-progress.js
@@ -1,22 +1,24 @@
 /**
 @license
 Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+This code may only be used under the BSD style license found at
+http://polymer.github.io/LICENSE.txt The complete set of authors may be found at
+http://polymer.github.io/AUTHORS.txt The complete set of contributors may be
+found at http://polymer.github.io/CONTRIBUTORS.txt Code distributed by Google as
+part of the polymer project is also subject to an additional IP rights grant
+found at http://polymer.github.io/PATENTS.txt
 */
 import '@polymer/polymer/polymer-legacy.js';
-
 import '@polymer/iron-flex-layout/iron-flex-layout.js';
-import { IronRangeBehavior } from '@polymer/iron-range-behavior/iron-range-behavior.js';
 import '@polymer/paper-styles/color.js';
-import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
-import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+
+import {IronRangeBehavior} from '@polymer/iron-range-behavior/iron-range-behavior.js';
+import {Polymer} from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import {html} from '@polymer/polymer/lib/utils/html-tag.js';
 
 /**
-Material design: [Progress & activity](https://www.google.com/design/spec/components/progress-activity.html)
+Material design: [Progress &
+activity](https://www.google.com/design/spec/components/progress-activity.html)
 
 The progress bars are for situations where the percentage completed can be
 determined. They give users a quick sense of how much longer an operation
@@ -70,19 +72,30 @@ To change the duration of the indeterminate cycle:
 
 The following mixins are available for styling:
 
-Custom property                                  | Description                                 | Default
+Custom property                                  | Description | Default
 -------------------------------------------------|---------------------------------------------|--------------
-`--paper-progress-container`                     | Mixin applied to container                  | `{}`
-`--paper-progress-transition-duration`           | Duration of the transition                  | `0.08s`
-`--paper-progress-transition-timing-function`    | The timing function for the transition      | `ease`
-`--paper-progress-transition-delay`              | delay for the transition                    | `0s`
-`--paper-progress-container-color`               | Color of the container                      | `--google-grey-300`
-`--paper-progress-active-color`                  | The color of the active bar                 | `--google-green-500`
-`--paper-progress-secondary-color`               | The color of the secondary bar              | `--google-green-100`
-`--paper-progress-disabled-active-color`         | The color of the active bar if disabled     | `--google-grey-500`
-`--paper-progress-disabled-secondary-color`      | The color of the secondary bar if disabled  | `--google-grey-300`
-`--paper-progress-height`                        | The height of the progress bar              | `4px`
-`--paper-progress-indeterminate-cycle-duration`  | Duration of an indeterminate cycle          | `2s`
+`--paper-progress-container`                     | Mixin applied to container |
+`{}`
+`--paper-progress-transition-duration`           | Duration of the transition |
+`0.08s`
+`--paper-progress-transition-timing-function`    | The timing function for the
+transition      | `ease`
+`--paper-progress-transition-delay`              | delay for the transition |
+`0s`
+`--paper-progress-container-color`               | Color of the container |
+`--google-grey-300`
+`--paper-progress-active-color`                  | The color of the active bar
+| `--google-green-500`
+`--paper-progress-secondary-color`               | The color of the secondary
+bar              | `--google-green-100`
+`--paper-progress-disabled-active-color`         | The color of the active bar
+if disabled     | `--google-grey-500`
+`--paper-progress-disabled-secondary-color`      | The color of the secondary
+bar if disabled  | `--google-grey-300`
+`--paper-progress-height`                        | The height of the progress
+bar              | `4px`
+`--paper-progress-indeterminate-cycle-duration`  | Duration of an indeterminate
+cycle          | `2s`
 
 @group Paper Elements
 @element paper-progress
