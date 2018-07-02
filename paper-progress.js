7f642a67d9e91285cf9cee27711ae26aa658bfcd
Keanu Lee <keanulee@google.com>
2018-07-02 16:31:15 -0700

    Manual updates for 3.0

--- paper-progress.js
+++ paper-progress.js
@@ -7,6 +7,14 @@ The complete set of contributors may be found at http://polymer.github.io/CONTRI
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 */
+import '@polymer/polymer/polymer-legacy.js';
+
+import '@polymer/iron-flex-layout/iron-flex-layout.js';
+import { IronRangeBehavior } from '@polymer/iron-range-behavior/iron-range-behavior.js';
+import '@polymer/paper-styles/color.js';
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+
 /**
 Material design: [Progress & activity](https://www.google.com/design/spec/components/progress-activity.html)
 
@@ -81,18 +89,6 @@ Custom property                                  | Description                  
 @hero hero.svg
 @demo demo/index.html
 */
-/*
-  FIXME(polymer-modulizer): the above comments were extracted
-  from HTML and may be out of place here. Review them and
-  then delete this comment!
-*/
-import '@polymer/polymer/polymer-legacy.js';
-
-import '@polymer/iron-flex-layout/iron-flex-layout.js';
-import { IronRangeBehavior } from '@polymer/iron-range-behavior/iron-range-behavior.js';
-import '@polymer/paper-styles/color.js';
-import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
-import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 Polymer({
   _template: html`
     <style>
